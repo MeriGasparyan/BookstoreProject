f91d56461dfc062ada5fe8e11f24b1e832ce02d8
MeriGasparyan <merigas04@gmail.com>
2025-04-02 19:27:16 +0400

    Add Jakarta dependency

--- src/main/java/org/example/bookstoreproject/persistance/entry/Author.java
+++ src/main/java/org/example/bookstoreproject/persistance/entry/Author.java
@@ -1,4 +1,19 @@
 package org.example.bookstoreproject.persistance.entry;
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
 
+@Entity
+@Table(name = "author")
+@Setter
+@Getter
 public class Author {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String title;
+
+    private Long authorId;
 }
