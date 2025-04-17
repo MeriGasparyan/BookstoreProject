c63d7f54f581db99b03a9322819108eb38ea5816
MeriGasparyan <merigas04@gmail.com>
2025-04-17 18:33:07 +0400

    Modify book adding functionality

--- src/main/java/org/example/bookstoreproject/service/dto/BookCreateRequestDTO.java
+++ src/main/java/org/example/bookstoreproject/service/dto/BookCreateRequestDTO.java
@@ -2,6 +2,15 @@ package org.example.bookstoreproject.service.dto;
 
 import lombok.Getter;
 import lombok.Setter;
+import org.example.bookstoreproject.enums.Format;
+import org.example.bookstoreproject.enums.Language;
+import org.example.bookstoreproject.persistance.entry.Book;
+import org.example.bookstoreproject.persistance.entry.Publisher;
+import org.example.bookstoreproject.persistance.entry.Series;
+import org.example.bookstoreproject.persistance.repository.*;
+
+import java.util.Date;
+import java.util.List;
 
 @Getter
 @Setter
@@ -9,23 +18,60 @@ public class BookCreateRequestDTO {
     private String title;
     private String bookID;
     private String series;
-    private String author;
-    private String rating;
+    private List<String> author;
     private String description;
     private String language;
     private String isbn;
-    private String genres;
-    private String characters;
+    private List<String> genres;
+    private List<String> characters;
     private String format;
     private String edition;
-    private String pages;
+    private Integer pages;
     private String publisher;
-    private String publishDate;
-    private String firstPublishDate;
-    private String awards;
-    private String ratingsByStar;
-    private String settings;
-    private String bbeScore;
-    private String bbeVotes;
-    private String price;
+    private Date publishDate;
+    private Date firstPublishDate;
+    private List<String> awards;
+    private List<String> settings;
+    private Integer bbeScore;
+    private Integer bbeVotes;
+    private Float price;
+
+
+    public Book createBookEntity(BookCreateRequestDTO createRequest, PublisherRepository publisherRepository, SeriesRepository seriesRepository) {
+        Book book = new Book();
+        book.setBookID(createRequest.getBookID());
+        book.setTitle(createRequest.getTitle());
+        book.setIsbn(createRequest.getIsbn());
+
+        if (createRequest.getLanguage() != null) {
+            book.setLanguage(Language.fromString(createRequest.getLanguage()));
+        }
+
+        if (createRequest.getFormat() != null) {
+            book.setFormat(Format.fromString(createRequest.getFormat()));
+        }
+
+        book.setPages(createRequest.getPages());
+        book.setPrice(createRequest.getPrice());
+        book.setBbeScore(createRequest.getBbeScore());
+        book.setBbeVotes(createRequest.getBbeVotes());
+        book.setPublishDate(createRequest.getPublishDate());
+        book.setFirstPublishDate(createRequest.getFirstPublishDate());
+
+        if (createRequest.getPublisher() != null) {
+            book.setPublisher(
+                    publisherRepository.findByName(createRequest.getPublisher())
+                            .orElseGet(() -> publisherRepository.save(new Publisher(createRequest.getPublisher())))
+            );
+        }
+
+        if (createRequest.getSeries() != null) {
+            book.setSeries(
+                    seriesRepository.findByTitle(createRequest.getSeries())
+                            .orElseGet(() -> seriesRepository.save(new Series(createRequest.getSeries())))
+            );
+        }
+
+        return book;
+    }
 }
--- src/main/java/org/example/bookstoreproject/service/services/BookService.java
+++ src/main/java/org/example/bookstoreproject/service/services/BookService.java
@@ -1,47 +1,28 @@
 package org.example.bookstoreproject.service.services;
 
 import lombok.AllArgsConstructor;
-import org.example.bookstoreproject.enums.Format;
-import org.example.bookstoreproject.enums.Language;
-import org.example.bookstoreproject.enums.RatingStarNumber;
 import org.example.bookstoreproject.persistance.entry.*;
 import org.example.bookstoreproject.persistance.entry.Character;
 import org.example.bookstoreproject.persistance.repository.*;
 import org.example.bookstoreproject.service.dto.BookCreateRequestDTO;
 import org.example.bookstoreproject.service.dto.BookDTO;
 import org.example.bookstoreproject.service.dto.BookSearchRequestDTO;
-import org.example.bookstoreproject.service.format.FloatFormatter;
-import org.example.bookstoreproject.service.format.IntegerFormatter;
-import org.example.bookstoreproject.service.format.RatingByStarFormatter;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.*;
 import java.util.stream.Collectors;
-import java.text.ParseException;
-import java.text.SimpleDateFormat;
 
 @Service
 @AllArgsConstructor
 public class BookService {
     private final BookRepository bookRepository;
-    private final BookAuthorRepository bookAuthorRepository;
-    private final BookAwardRepository bookAwardRepository;
-    private final BookCharacterRepository bookCharacterRepository;
-    private final BookGenreRepository bookGenreRepository;
-    private final BookSettingRepository bookSettingRepository;
     private final AuthorRepository authorRepository;
-
     private final SeriesRepository seriesRepository;
     private final PublisherRepository publisherRepository;
-    private final RatingStarRepository ratingStarRepository;
-    private final StarRepository starRepository;
     private final CharacterRepository characterRepository;
     private final GenreRepository genreRepository;
     private final AwardRepository awardRepository;
-    private final RatingByStarFormatter ratingByStarFormatter;
-    private final IntegerFormatter integerFormatter;
-    private final FloatFormatter floatFormatter;
     private final SettingRepository settingRepository;
 
     @Transactional
@@ -50,10 +31,8 @@ public class BookService {
             throw new IllegalArgumentException("Book with ID " + createRequest.getBookID() + " already exists.");
         }
 
-        Book book = createBookEntity(createRequest);
+        Book book = createRequest.createBookEntity(createRequest, publisherRepository, seriesRepository);
         bookRepository.save(book);
-
-        addRatingStars(book, createRequest.getRatingsByStar());
         addAuthors(book, createRequest.getAuthor());
         addCharacters(book, createRequest.getCharacters());
         addGenres(book, createRequest.getGenres());
@@ -61,98 +40,58 @@ public class BookService {
         addSettings(book, createRequest.getSettings());
     }
 
-    private Book createBookEntity(BookCreateRequestDTO createRequest) {
-        Book book = new Book();
-        book.setBookID(createRequest.getBookID());
-        book.setTitle(createRequest.getTitle());
-        book.setIsbn(createRequest.getIsbn());
-        book.setLanguage(Language.fromString(createRequest.getLanguage()));
-        book.setFormat(Format.fromString(createRequest.getFormat()));
-        book.setPages(integerFormatter.getInt(createRequest.getPages()));
-        book.setPrice(floatFormatter.getFloat(createRequest.getPrice()));
-        book.setBbeScore(integerFormatter.getInt(createRequest.getBbeScore()));
-        book.setBbeVotes(integerFormatter.getInt(createRequest.getBbeVotes()));
-
-
-        SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd");
-        try {
-            if (createRequest.getPublishDate() != null && !createRequest.getPublishDate().isEmpty()) {
-                book.setPublishDate(dateFormat.parse(createRequest.getPublishDate()));
-            }
-            if (createRequest.getFirstPublishDate() != null && !createRequest.getFirstPublishDate().isEmpty()) {
-                book.setFirstPublishDate(dateFormat.parse(createRequest.getFirstPublishDate()));
-            }
-        } catch (ParseException e) {
-            throw new IllegalArgumentException("Invalid date format. Please use yyyy-MM-dd.");
-        }
-
-        book.setPublisher(
-                publisherRepository.findByName(createRequest.getPublisher())
-                        .orElseGet(() -> publisherRepository.save(new Publisher(createRequest.getPublisher())))
-        );
-        book.setSeries(
-                seriesRepository.findByTitle(createRequest.getSeries())
-                        .orElseGet(() -> seriesRepository.save(new Series(createRequest.getSeries())))
-        );
-
-        return book;
-    }
-
-    private void addRatingStars(Book book, String ratingsByStar) {
-        Map<RatingStarNumber, Long> starMap = ratingByStarFormatter.formatRatingsByStar(ratingsByStar);
-        starMap.forEach((starLevel, count) -> {
-            Star star = starRepository.findByLevel(starLevel.name())
-                    .orElseGet(() -> starRepository.save(new Star(starLevel.name())));
-            ratingStarRepository.save(new BookRatingStar(book, star, count));
-        });
-    }
-
-    private void addAuthors(Book book, String authors) {
-        Arrays.stream(Optional.ofNullable(authors).orElse("").split(","))
-                .filter(name -> !name.trim().isEmpty())
+    private void addAuthors(Book book, List<String> authors) {
+        Optional.ofNullable(authors).orElse(List.of()).stream()
+                .map(String::trim)
+                .filter(name -> !name.isEmpty())
                 .forEach(name -> {
-                    Author author = authorRepository.findByName(name.trim())
-                            .orElseGet(() -> authorRepository.save(new Author(name.trim())));
+                    Author author = authorRepository.findByName(name)
+                            .orElseGet(() -> authorRepository.save(new Author(name)));
                     book.addBookAuthor(new BookAuthor(book, author));
                 });
     }
 
-    private void addCharacters(Book book, String characters) {
-        Arrays.stream(Optional.ofNullable(characters).orElse("").split(","))
-                .filter(name -> !name.trim().isEmpty())
+
+    private void addCharacters(Book book, List<String> characters) {
+        Optional.ofNullable(characters).orElse(List.of()).stream()
+                .map(String::trim)
+                .filter(name -> !name.isEmpty())
                 .forEach(name -> {
-                    Character character = characterRepository.findByName(name.trim())
-                            .orElseGet(() -> characterRepository.save(new Character(name.trim())));
+                    Character character = characterRepository.findByName(name)
+                            .orElseGet(() -> characterRepository.save(new Character(name)));
                     book.addBookCharacter(new BookCharacter(book, character));
                 });
     }
 
-    private void addGenres(Book book, String genres) {
-        Arrays.stream(Optional.ofNullable(genres).orElse("").split(","))
-                .filter(name -> !name.trim().isEmpty())
+    private void addGenres(Book book, List<String> genres) {
+        Optional.ofNullable(genres).orElse(List.of()).stream()
+                .map(String::trim)
+                .filter(name -> !name.isEmpty())
                 .forEach(name -> {
-                    Genre genre = genreRepository.findByName(name.trim())
-                            .orElseGet(() -> genreRepository.save(new Genre(name.trim())));
+                    Genre genre = genreRepository.findByName(name)
+                            .orElseGet(() -> genreRepository.save(new Genre(name)));
                     book.addBookGenre(new BookGenre(book, genre));
                 });
     }
 
-    private void addAwards(Book book, String awards) {
-        Arrays.stream(Optional.ofNullable(awards).orElse("").split(","))
-                .filter(name -> !name.trim().isEmpty())
+    private void addAwards(Book book, List<String> awards) {
+        Optional.ofNullable(awards).orElse(List.of()).stream()
+                .map(String::trim)
+                .filter(name -> !name.isEmpty())
                 .forEach(name -> {
-                    Award award = awardRepository.findByTitle(name.trim())
-                            .orElseGet(() -> awardRepository.save(new Award(name.trim())));
+                    Award award = awardRepository.findByTitle(name)
+                            .orElseGet(() -> awardRepository.save(new Award(name)));
                     book.addBookAward(new BookAward(book, award));
                 });
     }
 
-    private void addSettings(Book book, String settings) {
-        Arrays.stream(Optional.ofNullable(settings).orElse("").split(","))
-                .filter(name -> !name.trim().isEmpty())
+    private void addSettings(Book book, List<String> settings) {
+        Optional.ofNullable(settings).orElse(List.of()).stream()
+                .map(String::trim)
+                .filter(name -> !name.isEmpty())
                 .forEach(name -> {
-                    Setting setting = settingRepository.findByName(name.trim())
-                            .orElseGet(() -> settingRepository.save(new Setting(name.trim())));
+                    Setting setting = settingRepository.findByName(name)
+                            .orElseGet(() -> settingRepository.save(new Setting(name)));
                     book.addBookSetting(new BookSetting(book, setting));
                 });
     }
