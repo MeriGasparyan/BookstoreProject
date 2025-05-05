9062019815fe9dc5d7c0cf0b8b33f6ec6534c9d3
MeriGasparyan <merigas04@gmail.com>
2025-05-05 16:34:04 +0400

    Remove redundant comments

--- src/main/java/org/example/bookstoreproject/controller/BookController.java
+++ src/main/java/org/example/bookstoreproject/controller/BookController.java
@@ -186,30 +186,4 @@ public class BookController {
 
         return ResponseEntity.ok(response);
     }
-//    @GetMapping("/{id}/image")
-//    public ResponseEntity<InputStreamResource> getImage(
-//            @PathVariable("id") Long bookId,
-//            @RequestParam(value = "size", defaultValue = "original") String size
-//    ) {
-//        String imagePath = metadataService.getImagePath(bookId, ImageSize.fromString(size));
-//        if (imagePath == null) {
-//            return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
-//        }
-//
-//        try {
-//            File imageFile = new File(imagePath);
-//            if (!imageFile.exists()) {
-//                return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
-//            }
-//
-//            InputStream inputStream = new FileInputStream(imageFile);
-//            HttpHeaders headers = new HttpHeaders();
-//            headers.setContentType(MediaType.IMAGE_JPEG);
-//
-//            return new ResponseEntity<>(new InputStreamResource(inputStream), headers, HttpStatus.OK);
-//        } catch (IOException e) {
-//            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build();
-//        }
-//    }
-
 }
